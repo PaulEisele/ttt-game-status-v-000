61f5c0ce674f9719e1fb0cf5bff3dcc819bda95e
Paul Eisele <paulheisele@gmail.com>
2020-02-29 17:53:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,6 +22,12 @@ WIN_COMBINATIONS = [
 def won? (board)
   WIN_COMBINATIONS.detect do |win_combination|
 
+<<<<<<< HEAD
+=======
+#for each win_combination in WIN_COMBINATIONS
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+>>>>>>> 7eaa8c19a11c48845d729869b465724946e0954e
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
@@ -41,6 +47,7 @@ def won? (board)
 end
 
 def full?(board)
+<<<<<<< HEAD
   if board.detect {|i| i == " " || i == nil}
     return false
   else
@@ -49,6 +56,16 @@ def full?(board)
 end
 
 def draw? (board)
+=======
+  full_board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+full?(full_board) #=> true
+
+incomplete_board = ["X", " ", "X", "O", " ", "X", "O", " ", "O"]
+full?(incomplete_board) #=> false
+end
+
+def draw?
+>>>>>>> 7eaa8c19a11c48845d729869b465724946e0954e
   if !won?(board) && full?(board)
     return true
   else
@@ -56,6 +73,7 @@ def draw? (board)
   end
 end
 
+<<<<<<< HEAD
 def over? (board)
   if won?(board) || draw?(board)
     return true
@@ -68,4 +86,26 @@ def winner(board)
   if win_combination = won?(board)
     board[win_combination.first]
   end
+=======
+def over?
+  draw_board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+over?(draw_board) #=> true
+
+won_board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
+over?(won_board) #=> true
+
+inprogress_board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+over?(inprogress_board) #=> false
+end
+
+def winner?
+  x_win_diagonal = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+  winner(x_win_diagonal) #=> "X"
+
+  o_win_center_column = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+  winner(o_win_center_column) #=> "O"
+
+  no_winner_board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
+  winner(no_winner_board) #=> nil
+>>>>>>> 7eaa8c19a11c48845d729869b465724946e0954e
 end
